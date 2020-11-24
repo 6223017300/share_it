11165b5bff0cde0ea40bdf4d87b022756ec2c715
djena <djenabkouradia1@gmail.com>
2020-11-24 10:19:43 +0100

    AJOUTE la configuration minimal du projet
    Configure la connexion à la base de données, le template de base
    Twig et les routes de chargement et telechargement de fichiers.

--- public/index.php
+++ public/index.php
@@ -12,11 +12,34 @@ use Slim\App;
 // App configuration
 require_once __DIR__ . '/../config/bootstrap.php';
 
-// Application routes
 $app
+    ->map(['GET', 'POST'], '/', [HomeController::class, 'homepage'])
+    ->setName('homepage')
+;
+
+
+$app    
+    ->get('/download/{id:\d+}', [HomeController::class, 'download'])
+    ->setName('download')
+;
+
+// Application routes
+/*$app
     ->get('/', [HomeController::class, 'homepage'])
     ->setName('homepage')
 ;
+
+$app
+    ->map(['GET', 'POST'], '/test', [HomeController::class, 'test'])
+    ->setName('test')
+;
+
+$app
+    ->get('/a-propos', [HomeController::class, 'about'])
+    ->setName('about')
+;*/
+
+
 
 // Start the application
 $app->run();
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,35 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
 use Psr\Http\Message\ResponseInterface;
 
 class HomeController extends AbstractController
 {
-    public function homepage(ResponseInterface $response)
+    public function homepage(ResponseInterface $response, Connection $connection)
     {
-        return $this->template($response, 'homepage.html.twig');
+        //$database = $connection->getDatabase();
+
+        return $this->template($response, 'home.html.twig' );
+            //'database_name' => $database,
+            //'users' => ['Pierre', 'Paul', 'Jacque'],
+            //'xss' => '<script> window.alert ("Coucou"); </script>'
+       
+    }
+
+   /* public function test(){
+
+        return $this->redirect('about');
+    }
+
+    public function about(ResponseInterface $response){
+
+        $response->getBody()->write('<h1> A propos de nous </h1>');
+        return $response;
+    }*/
+
+    public function download(ResponseInterface $response, int $id){
+        $response->getBody()->write(sprintf('Identifiant: %d', $id));
+        return $response;
     }
 }
